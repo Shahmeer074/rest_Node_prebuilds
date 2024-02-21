c1338fa2e6fc688218d087f66eb4cdc28399946e
Shahmeer074 <160682508+Shahmeer074@users.noreply.github.com>
2024-02-21 14:35:27 +0500

    Update ModelExample.js

--- ModelExample.js
+++ ModelExample.js
@@ -1,3 +1,5 @@
+//categories scehma
+
 // const { Timestamp } = require("bson");
 //const { timeStamp } = require("console");
 const mongoose = require("mongoose");
@@ -20,3 +22,31 @@ const CategoriesSchema = new Schema(
 
 
 module.exports = mongoose.model('Categories', CategoriesSchema);
+
+
+//subcategories schema
+
+
+
+// const { Timestamp } = require("bson");
+//const { timeStamp } = require("console");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SubcategoriesSchema = new Schema(
+  {
+    name: {
+      String,
+      required: true,
+    },
+
+    Category:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Categories',
+    }]
+  }
+);
+
+
+module.exports = mongoose.model('Subcategories', SubcategoriesSchema);
+
